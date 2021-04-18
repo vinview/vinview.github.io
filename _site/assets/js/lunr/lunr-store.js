48ac42796d55d457c6a5a48cdec49c11587ace99
vinview <vinview@gmail.com>
2021-04-18 12:07:19 +0530

    initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,101 @@
+var store = [{
+        "title": "Personal Website! Mission Accomplished :)",
+        "excerpt":"As an undergrad, I learned some basic HTML and CSS and created a simple webpage in a class project. I haven’t thought back then that I would have a fully functional personal website one day with all the modern features like social sharing, tagging, related posts and so on. All...","categories": [],
+        "tags": ["Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2019/08/28/Personal-Website-Mission-accomplished.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/my-website-thumb-400.jpg"},{
+        "title": "Personal Website with Minimal Mistakes Jekyll Theme HOWTO - Part I",
+        "excerpt":"This is Part I of the website HOWTO series. See Part II on theme customization, Part III on deployment on GitHub pages and Part IV on SEO and analytics.     This website is built with Jekyll using Minimal Mistakes theme and hosted on GitHub Pages. For development and writing...","categories": [],
+        "tags": ["Docker","Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2019/08/28/Personal-website-with-Minimal-Mistakes-Jekyll-Theme-HOWTO-Part-I.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Personal Website with Minimal Mistakes Jekyll Theme HOWTO - Part II",
+        "excerpt":"This is Part II of the website HOWTO series. See Part I on Docker, Part III on deployment with GitHub pages and Part IV on SEO and analytics.     This website is built with Jekyll using Minimal Mistakes theme and hosted on GitHub Pages. For development and writing posts...","categories": [],
+        "tags": ["Minimal Mistakes Jekyll","CSS"],
+        "url": "http://localhost:4000/2019/08/28/Personal-website-with-Minimal-Mistakes-Jekyll-Theme-HOWTO-Part-II.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Personal Website with Minimal Mistakes Jekyll Theme HOWTO - Part III",
+        "excerpt":"This is Part III of the website HOWTO series. See Part I on Docker and Part II on theme customization and Part IV on SEO and analytics.     This website is built with Jekyll using Minimal Mistakes theme and hosted on GitHub Pages. For development and writing posts I...","categories": [],
+        "tags": ["Minimal Mistakes Jekyll","GitHub Pages","DNS"],
+        "url": "http://localhost:4000/2019/08/28/Personal-website-with-Minimal-Mistakes-Jekyll-Theme-HOWTO-Part-III.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Personal Website with Minimal Mistakes Jekyll Theme HOWTO - Part IV",
+        "excerpt":"This is Part IV of the website HOWTO series. See Part I on Docker, Part II on theme customization and Part III on deployment on GitHub Pages with private domain.     This website is built with Jekyll using Minimal Mistakes theme and hosted on GitHub Pages. For development and...","categories": [],
+        "tags": ["Minimal Mistakes Jekyll","Twitter Cards","Facebook OG","Disqus","Google Analytics","SEO","Google Search Console","LinkedIn"],
+        "url": "http://localhost:4000/2019/09/01/Personal-website-with-Minimal-Mistakes-Jekyll-Theme-HOWTO-Part-IV.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Insight Data Science Fellows: Who are They?",
+        "excerpt":"INSIGHT Data Science The INSIGHT Data Science Fellows Program is a competitive fellowship targeted at academics from the top universities. It helps recent PhDs and Postdocs to find a prestigious job as data scientists in the industry. During 7 weeks of the program, the fellows work on their own data...","categories": [],
+        "tags": ["Insight Data Science Fellowship"],
+        "url": "http://localhost:4000/2019/09/03/Insight-Data-Science-Fellows.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/insight-data-science-thumb-600.jpg"},{
+        "title": "Target Audience for Direct Marketing in Starbucks Rewards Mobile App",
+        "excerpt":"Photo by unsplash-logoDaiga Ellaby This is the technical report I wrote for the capstone project in Udacity Data Science Nanodegree. The code for this project can be found on my GitHub here. 1. Project Definition 1.1. Project Overview In this project I analyze the customer behavior in the Starbucks rewards...","categories": [],
+        "tags": ["Udacity Data Science Nanodegree","customer segmentation","K-means & PCA"],
+        "url": "http://localhost:4000/2019/09/19/Starbucks-Rewards-Program.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/daiga-ellaby-unsplash-600.jpg"},{
+        "title": "Udacity Nanodegrees compared: Data Science vs Intro to Machine Learning",
+        "excerpt":"Recently I graduated from Udacity Data Science Nanodegree, which was my second program with this MOOC platform. 1 Because Udacity restructured the program I took, this post is basically the review of two programs - Udacity Data Science Nanodegree and Intro to Machine Learning Nanodegree. Due to restructuring, Data Science...","categories": [],
+        "tags": ["Udacity Data Science Nanodegree"],
+        "url": "http://localhost:4000/2019/10/01/Udacity-Data-Science-Nanodegree.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/udacity-ds-nanodegree-800.png"},{
+        "title": "How to install Google Optimize on your Minimal Mistakes website",
+        "excerpt":"In this post I describe how I installed Google Optimize on my Minimal Mistakes website so that I can run A/B tests. 1. Register with Google Optimize and create a container which will hold all the Optimize configurations for your A/B tests. You get a container ID that starts with...","categories": [],
+        "tags": ["A/B Testing with Google Optimize","Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2020/02/26/How-to-install-Google-Optimize.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "How to link Google Analytics and Google Search Console",
+        "excerpt":"In another post I described how to register your Minimal Mistakes website with Google Analytics and Google Search Console. It turns out that I missed one crucial step - linking them together. Here’s a HOWTO do it.     Why linking Google Search Console with Google Analytics? Google Search Console...","categories": [],
+        "tags": ["Google Analytics","Google Search Console","Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2020/02/28/How-to-link-Google-Analytics-and-Google-Search-Console.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Setting up A/B Test with Google Optimize - Example",
+        "excerpt":"In this post I describe #how I set up my first A/B test using Google Optimize. If you haven’t yet installed it on your Minimal Mistakes website, you can read the #howto here. Stating the Objective A/B test is a randomized experiment to test two variants in order to decide...","categories": [],
+        "tags": ["A/B Testing with Google Optimize","Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2020/02/28/Setting-Up-AB-test-with-Google-Optimize.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "How to render math equations on your Minimal Mistakes",
+        "excerpt":"You need MathJax if you want to render mathematical equations on your Minimal Mistakes website. I first learned about it from the blog post by Peter Willis (thanks, Peter!) However, I couldn’t enable MathJax even after following his instructions. So here’s what I did. 1. Add this snippet at the...","categories": [],
+        "tags": ["MathJax","Minimal Mistakes Jekyll"],
+        "url": "http://localhost:4000/2020/03/23/How-to-render-math-on-Minimal-Mistakes.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Modeling COVID-19 in Silicon Valley",
+        "excerpt":"Photo by unsplash-logoCDC A simple model of coronavirus spread in Silicon Valley shows that practicing social distance might help in slowing the spread of coronavirus. But if the growth rate doesn’t change, then in May we will face a critical situation similar to Italy in March.     On March...","categories": [],
+        "tags": ["COVID-19 in Bay Area"],
+        "url": "http://localhost:4000/2020/03/23/Modeling-Coronavirus-in-Silicon-Valley.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/coronavirus-unsplash-600.jpg"},{
+        "title": "My Experience with Insight Fellowship",
+        "excerpt":"INSIGHT Data Science Somebody posted a question today on a forum about the Insight Fellowship. This prompted me to write about my experience with Insight Data Science. I applied to the INSIGHT Data Science in San Francisco three times in 2019, went through 5 interviews and still was rejected. Some...","categories": [],
+        "tags": ["Insight Data Science Fellowship"],
+        "url": "http://localhost:4000/2020/04/06/Insight-Fellowship-Review.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/insight-data-science-thumb-600.jpg"},{
+        "title": "UPDATE: Modeling COVID-19 in Silicon Valley after 4 weeks of shelter-in-place order ",
+        "excerpt":"This is an UPDATE of my article “Modeling Coronavirus in Silicon Valley” from March 23, 2020. Photo by unsplash-logoCDC     April 7 is the day when people could have returned to normal life. It is the last day of the first shelter-in-place order in Santa Clara County before it...","categories": [],
+        "tags": ["COVID-19 in Bay Area"],
+        "url": "http://localhost:4000/2020/04/07/UPDATE-Modeling-Coronavirus-in-Silicon-Valley.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/coronavirus-unsplash-600.jpg"},{
+        "title": "How to Correctly Set Up Internal IP Filter in Google Analytics",
+        "excerpt":"Google Analytics allows you to filter out data based on IPs, which comes handy when you want to exclude your own views. It turned out that my internal IP filter did not work properly and my Google Analytics data all this time was contaminated by my own page views. :(...","categories": [],
+        "tags": ["Google Analytics","Minimal Mistakes Jekyll","Google Tag Assistant"],
+        "url": "http://localhost:4000/2020/04/21/How-to-Correctly-Set-Up-Internal-IP-Filter-in-Google-Analytics.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "How to improve SEO by boosting website speed",
+        "excerpt":"Recently I taught myself how to conduct technical audit to improve SEO and want to share my knowledge here. My two reasons for speed slowdown were Image Sizes and Font Awesome. Read on to learn my solutions. Note: Although my solutions are universal, I have some specific advice for those...","categories": [],
+        "tags": ["Google Lighthouse","Minimal Mistakes Jekyll","SEO","PageSpeed Insights"],
+        "url": "http://localhost:4000/2020/04/27/How-to-improve-SEO-by-boosting-website-speed.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Troubleshooting: XML Sitemap when hosting on GitHub",
+        "excerpt":"TLDR: Don’t commit your auto-generated sitemap.xml to GitHub. It won’t be updated Why sitemap is important? Sitemaps encourage crawling. So if you want your site to be “discovered” by Google, it’s better to have an up-to-date sitemap. You can check your website’s indexing state in Google Search Console by going...","categories": [],
+        "tags": ["Google Search Console","Minimal Mistakes Jekyll","GitHub Pages"],
+        "url": "http://localhost:4000/2020/05/01/XML-Sitemap-Problem-when-hosting-on-GitHub.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Udacity Review: Digital Marketing Nanodegree",
+        "excerpt":"Udacity offered 1 month for free and I decided to enroll into Digital Marketing Nanodegree, mainly for two reasons. First, I am interested in Data Science for Marketing and Product Analytics, so I thought expanding my knowledge into Digital Marketing would help me better understand marketing problems. Second, I had...","categories": [],
+        "tags": ["Udacity Nanodegree Review","Digital Marketing"],
+        "url": "http://localhost:4000/2020/06/04/Udacity-Review-Digital-Marketing-Nanodegree.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"},{
+        "title": "Running Cassandra and PostgreSQL in Docker",
+        "excerpt":"Today I started thee Udacity Data Engineering Nanodegree and wanted to follow along the demo projects in Lesson 1 on my local machine. (However, this is optional as Udacity provides the environment in their workspace). In this post I describe how to set up the environment using Docker containers for...","categories": [],
+        "tags": ["Udacity Nanodegree","Data Engineering","Docker","Cassandra","PostgreSQL"],
+        "url": "http://localhost:4000/2020/07/27/Running-Cassandra-and-Postgres-in-Docker.html",
+        "teaser":"http://localhost:4000/assets/images/thumbnails/joel-filipe-thumb-400.jpg"}]
--- docs_old/VinayHegde.md
+++
@@ -1,3 +0,0 @@
-layout: page
-title: "VinayHegde"
-permalink: /name/
